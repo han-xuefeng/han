1b95cca604fe10aa1b58210f637914af9365a318
hanxuefeng <hanxuefeng@netjoy.com>
2021-10-13 11:46:14 +0800

    实现钩子

---
+++ han-orm/day5-hooks/session/raw.go
@@ -0,0 +1,81 @@
+package session
+
+import (
+	"database/sql"
+	"geeorm/clause"
+	"geeorm/dialect"
+	"geeorm/log"
+	"geeorm/schema"
+	"strings"
+)
+
+type Session struct {
+	db *sql.DB
+	dialect  dialect.Dialect  // 数据库的操作类  如mysql  sqlite3
+	tx       *sql.Tx // 事务
+	refTable *schema.Schema //对应的schema 这里是对应的表
+	clause   clause.Clause
+	sql strings.Builder
+	sqlVars []interface{}
+}
+
+type CommonDB interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+var _ CommonDB = (*sql.DB)(nil)
+var _ CommonDB = (*sql.Tx)(nil)
+
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+func (s *Session) DB() CommonDB {
+	if s.tx != nil {
+		return s.tx
+	}
+	return s.db
+}
+
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+func (s *Session)Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// QueryRow gets a record from db
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// QueryRows gets a list of records from db
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
---
+++ han-orm/day5-hooks/session/table_test.go
@@ -0,0 +1,20 @@
+package session
+
+import (
+	"testing"
+)
+
+type User struct {
+	Name string `geeorm:"PRIMARY KEY"`
+	Age  int
+}
+
+
+func TestSession_CreateTable(t *testing.T) {
+	s := NewSession().Model(&User{})
+	_ = s.DropTable()
+	_ = s.CreateTable()
+	if !s.HasTable() {
+		t.Fatal("Failed to create table User")
+	}
+}
